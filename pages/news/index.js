151ad7347ffdb8bd3a16d8129e86197b68d9cf1e
== <prantocse01@gmail.com>
2022-03-20 13:08:33 +0600

    Linking Between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,16 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 //domain.com/news
 function NewsPage() {
-    return <h1>The News page</h1>
+  return (
+    <Fragment>
+      <h1>The News page</h1>
+      <ul>
+          <li><Link href="/news/nextjs-is-a-great-freamework">NextJS is  A Great Framework</Link></li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
