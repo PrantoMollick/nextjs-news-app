87df33bf1943a0c0dc92f9183b2bed66be36746b
== <prantocse01@gmail.com>
2022-03-20 15:30:16 +0600

    custom css

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,14 @@
-const HomePage = (params) => {
-  
-}
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg'
+      title='A first Meetup'
+      address='some street 5, some city'
+      description='The meetup description'
+    />
+  );
+};
+
+export default MeetupDetails;
