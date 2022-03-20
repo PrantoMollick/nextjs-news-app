005fabaaa9a7455c977c0a945f21f1b8a6ca1421
== <prantocse01@gmail.com>
2022-03-21 00:17:05 +0600

    working with mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,9 +1,21 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 //post /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
+        // const {title, image, address, description} = data;
+
+        const client = await MongoClient.connect('mongodb+srv://pranto:1y1lYALHhbGFI5ry@cluster0.q0s4p.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne(data);
+        console.log(result);
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
 
